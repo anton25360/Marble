81ddd29a462418ce8e3017e210e5a553c69bcaf7
Anton Webb <anton25360@gmail.com>
2019-10-08 14:55:13 +0100

    Make entered text gradient.

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ document.querySelector('form').addEventListener('submit', function(event) {
 
     let color1 = document.querySelector('[name="color1"]').value
     let color2 = document.querySelector('[name="color2"]').value
+    let text = document.querySelector('[name="text"]').value
+    let bg = document.querySelector('[name="bg"]').value
 
-    console.log(color1);
-    console.log(color2);
     
     document.querySelector('.container').style.backgroundImage = "linear-gradient(to right, "+ color1 +", "+ color2 +")"
+
+    document.querySelector('.textTest').innerHTML = "<h1 style='background: linear-gradient(to right,"+ color1 +", "+ color2 +"); display:inline-block; background-clip: text; -webkit-text-fill-color: transparent; font-size:150px;'>"+text+"</h1>"
+
+    // background: linear-gradient(to right,#FF5F6D,#FFC371);
+
+
 })
