bde9c724f66cfa5fa0cd0fbfdb2edad40748c204
Anton Webb <anton25360@gmail.com>
2019-10-10 14:58:29 +0100

    Revert to 65e45a877e08be24e99708c2c264efadce97a4c7

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ document.querySelector('form').addEventListener('submit', function(event) {
     let bg = document.querySelector('[name="bg"]').value
 
     //gradient strip
-    document.querySelector('.gradientStrip').style.backgroundImage = "linear-gradient(to right, " + color1 + ", " + color2 + ")"
+    document.querySelector('.gradientStrip').style.backgroundImage = "linear-gradient(to right, "+ color1 +", "+ color2 +")"
 
     //badge content
-    document.querySelector('.badge').innerHTML = "<h1 style='background: linear-gradient(to right," + color1 + ", " + color2 + "); display:inline-block; background-clip:text; -webkit-background-clip: text; -webkit-text-fill-color:transparent; font-size:80px; margin:0; padding:30px 70px;'>" + text + "</h1>"
+    document.querySelector('.badge').innerHTML = "<h1 style='background: linear-gradient(to right,"+ color1 +", "+ color2 +"); display:inline-block; background-clip:text; -webkit-background-clip: text; -webkit-text-fill-color:transparent; font-size:80px; margin:0; padding:30px 70px;'>"+text+"</h1>"
 
     //badge color
     if (bg == 'dark') {
@@ -19,5 +19,7 @@ document.querySelector('form').addEventListener('submit', function(event) {
     } else {
         document.querySelector('.badge').style.background = 'white'
     }
+    
+
 
 })
