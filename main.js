65e45a877e08be24e99708c2c264efadce97a4c7
Anton Webb <anton25360@gmail.com>
2019-10-10 14:22:04 +0100

    Made text bg opaque. changed some class names in JS.

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ document.querySelector('form').addEventListener('submit', function(event) {
     document.querySelector('.gradientStrip').style.backgroundImage = "linear-gradient(to right, "+ color1 +", "+ color2 +")"
 
     //badge content
-    document.querySelector('.textTest').innerHTML = "<h1 style='background: linear-gradient(to right,"+ color1 +", "+ color2 +"); display:inline-block; background-clip:text; -webkit-background-clip: text; -webkit-text-fill-color:transparent; font-size:80px; margin:0; padding:30px 70px;'>"+text+"</h1>"
+    document.querySelector('.badge').innerHTML = "<h1 style='background: linear-gradient(to right,"+ color1 +", "+ color2 +"); display:inline-block; background-clip:text; -webkit-background-clip: text; -webkit-text-fill-color:transparent; font-size:80px; margin:0; padding:30px 70px;'>"+text+"</h1>"
 
     //badge color
     if (bg == 'dark') {
-        document.querySelector('.textTest').style.background = 'black'
+        document.querySelector('.badge').style.background = 'black'
     } else {
-        document.querySelector('.textTest').style.background = 'white'
+        document.querySelector('.badge').style.background = 'white'
     }
     
 
